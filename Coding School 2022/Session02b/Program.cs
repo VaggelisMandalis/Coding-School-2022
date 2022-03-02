409188c4038b8b0f70401a35daf96ec609022272
Evangelos Mandalis <evanmand@ea.gr>
2022-03-02 16:02:01 +0200

    First atttempt on second excercise

---
+++ Coding School 2022/Session02b/Program.cs
@@ -0,0 +1,40 @@
+﻿// 1)
+Console.WriteLine("Hello Vaggelis");
+
+// 2)
+int a = 5;
+int b = 6;
+int sum = a + b;
+double div = (double)a / b;
+Console.WriteLine("The sum is :" +sum);
+Console.WriteLine("The devision is :" + div);
+
+// 3)
+
+Console.WriteLine(-1 + 5 * 6);
+Console.WriteLine(38 + (5 % 7));
+Console.WriteLine(14 + (( - 3 * 6) / 7));
+Console.WriteLine(2+ 13/6 * 6 + Math.Sqrt(7));
+Console.WriteLine(Math.Pow(6,4) + Math.Pow(5, 7) / (9% 4));
+
+// 4)
+
+int age = 35;
+string gender = "Female";
+Console.WriteLine("You are "+ gender +" and you look younger than "+ age);
+
+//5)
+
+int secs = 25152;
+Console.WriteLine("There are " + (secs / 60) + " minutes, " + (secs / 3600) + "hours, " + (secs / 86400) + " days and " + (secs / 31556926) + " years.");
+
+//6)
+
+
+
+//7)
+int tempCel = 45;
+double tempKel = tempCel + 273;
+double TempFahr = tempCel * (18 / 10) + 32;
+Console.WriteLine("The temp in Kelvin is " + tempKel);
+Console.WriteLine("The temp in Fahreneit is  " + tempCel);
